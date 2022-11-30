4fb9aac0c7337e3bbb34d17431c553eeafdc79bf
ErikJJ <jive931221@gmail.com>
2022-11-29 23:30:42 -0600

    Proyecto final con algunas mejoras al final

--- js/app.js
+++ js/app.js
@@ -1,13 +1,11 @@
 let ingresos = [
   new Ingreso('trabajo extra', 1),
   new Ingreso('venta de ropa', 1),
-  
 ];
 const egresos = [
   new Egreso('Renta', 1),
   new Egreso('Ropa', 10)
 ];
-
 function cargarapp() {
   cargarCabecero();
   cargarIngresos();
@@ -22,7 +20,6 @@ const cargarCabecero = () => {
   document.getElementById('ingresos').innerHTML = formatoMoneda(totalIngreso());
   document.getElementById('egresos').innerHTML = formatoMoneda(totalEgresos());
 }
-
 const totalIngreso = () => {
   let totalIngreso = 0
   for (let ingreso of ingresos) {
@@ -50,7 +47,6 @@ const formatoPorcentaje = (value) => {
     style: "percent", minimumFractionDigits: 2
   });
 }
-
 //Ingreso
 const cargarIngresos = () => {
   let ingresosHTML = '';
@@ -59,7 +55,6 @@ const cargarIngresos = () => {
   }
   document.getElementById('lista-ingresos').innerHTML = ingresosHTML;
 }
-
 const crearIngresoHTML = (ingreso) => {
   let ingresosHTML = ` 
   <div class="elemento limpiarEstilos">
@@ -77,7 +72,6 @@ const crearIngresoHTML = (ingreso) => {
 `
   return ingresosHTML
 }
-
 //Eliminar datos de ingreso
 const eliminarIngreso = (id) => {
   var indiceEliminar = ingresos.findIndex(indice => indice.id === id)
@@ -95,7 +89,6 @@ const cargarEgreso = () => {
   }
   document.getElementById('lista-egresos').innerHTML = egresosHTML;
 }
-
 const crearEgresosHTML = (egreso) => {
 
   let egresosHTML = `
@@ -103,6 +96,7 @@ const crearEgresosHTML = (egreso) => {
 <div class="elemento_descripcion">${egreso.descripcion}</div>
 <div class="derecha limpiarEstilos">
     <div class="elemento_valor">-${formatoMoneda(egreso.valor)}</div>
+    <div class="elemento_porcentaje">${formatoPorcentaje(egreso.valor / totalEgresos())}</div>
     <div class="elemento_eliminar">
         <button class="elemento_eliminar--btn">
             <ion-icon name="close-circle-outline" 
@@ -121,57 +115,38 @@ const elimiarEgreso = (id) => {
   egresos.splice(indiceEliminar, 1)
   cargarEgreso();
   cargarCabecero();
-
 }
-
-
 //Agregar datos
 const agregarDatos = () => {
   var forma = document.getElementById("forma");
   var tipo = document.querySelector("#tipo").value;
   var descripcion = document.getElementById("descripcion").value;
   var valor = Number(document.getElementById("valor").value);
-
-
-
   if (descripcion === "" || valor <= 0) {
     if (descripcion === "") {
       alert("Ingresa la descripción")
     }
     else if (valor <= 0) {
       alert("El valor debe ser mayor a 0")
-      
     }
-   
   }
   else {
 
     if (tipo === "ingresos") {
-      agregar = new Ingreso(descripcion, valor)
-      baseDato()
+      //agregar = new Ingreso(descripcion, valor)
+      ingresos.push(new Ingreso(descripcion, valor));
+      //baseDato()
       cargarCabecero()
       cargarIngresos()
-
     }
     else {
-      agregaregresos = new Egreso(descripcion, valor)
-      baseDatoegresos()
+      egresos.push(new Egreso(descripcion,valor));
       cargarCabecero()
       cargarEgreso()
     }
     forma.reset()
   }
- 
 }
-
-function baseDato() {
-  ingresos.push(agregar)
-
-}
-function baseDatoegresos() {
-  egresos.push(agregaregresos)
-}
-
 
 
 
